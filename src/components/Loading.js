f7507fd4066dd05e690aebc23b11ef08c5c6d187
Hoang Dang Khanh <hoangdangkhanh12c1@gmail.com>
2021-06-23 15:42:11 +0700

    loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 export default function Loading(WithComp) {
     return class extends React.Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                isMount: false
+            }
+        }
         componentDidMount() {
             const loading = document.getElementById('loading')
-            loading.remove()
+            setTimeout(() => {
+                loading.remove()
+                this.setState({
+                    isMount: true
+                })
+            }, 1200)
         }
         render() {
+            if (!this.state.isMount) {
+                return null
+            }
             return(
                 <div>
                     <div class="Loading">
